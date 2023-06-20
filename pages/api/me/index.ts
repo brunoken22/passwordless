6ec21f6e3df686e1e6da00f4d79438a8ef4874fe
brunoken22 <bruno_am_22@hotmail.com>
2023-06-20 12:01:53 -0300

    commit

--- pages/api/me/index.ts
+++ pages/api/me/index.ts
@@ -3,14 +3,6 @@ import methods from "micro-method-router";
 import { authMiddelware } from "lib/middelware";
 import { User } from "lib/user";
 
-// export default methods({
-//    async get(req: NextApiRequest, res: NextApiResponse) {
-//       // const user = new User(token.id);
-//       // await user.pull();
-//       // res.send(user.data);
-//    },
-// });
-
 async function handler(req: NextApiRequest, res: NextApiResponse, token: any) {
    const user = new User(token.id);
    await user.pull();
---
+++ pages/api/test/index.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest,NextApiResponse} from 'next'
+import methods from 'micro-method-router'
+
+export default methods({
+   get(req:NextApiRequest,res:NextApiResponse){
+      res.send("Test")
+   }
+})
