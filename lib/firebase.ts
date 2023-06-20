1e369cfe9500471fdd3b64f620606ea2f84e21e5
brunoken22 <bruno_am_22@hotmail.com>
2023-06-20 14:31:19 -0300

    commit

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,9 +4,9 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_CONNECTION);
 // console.log(admin.app.length);
 
 if (!admin.app.length) {
-   admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-   });
 }
+admin.initializeApp({
+   credential: admin.credential.cert(serviceAccount),
+});
 const firebase = admin.firestore();
 export { firebase };
