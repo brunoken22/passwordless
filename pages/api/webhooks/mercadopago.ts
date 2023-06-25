4014f2ac49df3e5bfbf13ce11bec2c9f2f8d0a47
brunoken22 <bruno_am_22@hotmail.com>
2023-06-25 15:41:34 -0300

    commt

--- pages/api/webhooks/mercadopago.ts
+++ pages/api/webhooks/mercadopago.ts
@@ -3,8 +3,10 @@ import { Order } from "lib/order";
 
 export default async function (req, res) {
    const { id, topic } = req.body;
+   console.log(topic.id);
+
    if (topic == "merchant_order") {
-      const order = await getMerchantOrder(10028313247);
+      const order = await getMerchantOrder(id);
       if (order.order_status == "paid") {
          const orderId = order.external_reference;
          const newOrder = new Order(orderId);
