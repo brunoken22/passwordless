4960f00fc835b7fca017fc838fef10a3653e9549
brunoken22 <bruno_am_22@hotmail.com>
2023-06-25 13:50:16 -0300

    commit

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,7 +3,7 @@ import admin from "firebase-admin";
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONNECTION);
 // console.log(admin.app.length);
 
-if (!admin.app.length) {
+if (!admin.apps.length) {
    admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
    });
